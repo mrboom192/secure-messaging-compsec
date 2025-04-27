98b1a61dae70355291cd680e6f43c442e8f8834f
cristianae <Cristiana.M.Eagen-1@ou.edu>
2025-04-27 13:01:50 -0500

    implemented socket.io in the backend for real-time connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express"); // We're using express as our backend framework
 const app = express(); // Initialize our app instance
 const cors = require("cors");
+const http = require("http"); //implemented for socket.io server
+const {Server} = require("socket.io");
+const server = http.createServer(app);
+
 const corsOptions = {
   origin: ["http://localhost:5173"], // This is the port vite servers run on
 };
@@ -12,6 +16,27 @@ app.get("/api", (req, res) => {
   res.json({ fruits: ["apple", "orange", "banana"] });
 });
 
-app.listen(8080, () => {
+//socet.io server
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+//socket connection
+io.on("connection", (socket) =>{
+  console.log("A user connected: ${socket.id}");
+  socket.on("send_message", (data) => {
+    console.log("Message was received",data);
+    io.emit("receive_message", data); //send message to everyone
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ${socket.id}");
+  });
+});
+
+
+
+server.listen(8080, () => {
   console.log("Server started on port 8080");
 });
