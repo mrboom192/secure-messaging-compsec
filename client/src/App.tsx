055f96cee0979f0ceca261e2ed57215f174982d8
cristianae <Cristiana.M.Eagen-1@ou.edu>
2025-04-26 21:25:22 -0500

    layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,28 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Alice from "./assets/Alice.png";
+import Bob from "./assets/Bob.png";
+
 
 export default function App() {
+  const [aliceMessages, setAlice] = useState<string[]>([]);
+  const [bobMessages, setBob] = useState<string[]>([]);
+  const [aliceInput, setAliceInput] = useState("");
+  const [bobInput, setBobInput] = useState("");
+
+  const sendAlice = () => {
+    setAlice([...aliceMessages, aliceInput]);
+    setBob([...bobMessages, aliceInput]); 
+    setAliceInput("");
+  };
+
+  const sendBobMessage = () => {
+    setBob([...bobMessages, bobInput]);
+    setAlice([...aliceMessages, bobInput]);  // Alice receives Bob's message
+    setBobInput("");
+  };
+
+
   const fetchAPI = async () => {
     const response = await axios.get("http://localhost:8080/api");
     console.log(response.data.fruits);
@@ -11,9 +32,22 @@ export default function App() {
     fetchAPI();
   }, []);
 
+  // prev color #03dffc
+
   return (
-    <div className="bg-[#03dffc] w-screen h-screen">
-      <p className="font-bold text-3xl">Welcome to secure messaging!</p>
+    <div className="bg-[#38a9f5] w-screen h-screen">
+       <p className="font-bold text-3xl text-center">Welcome to secure messaging!</p>
+
+       <div className="flex w-full">
+       <div className="w-1/2 flex flex-col items-center">
+        <img src={Alice} alt="Alice" className="w-24 h-24 mb-2 rounded-full" />
+        <h2 className="font-bold text-2xl">Alice</h2>
+      </div>
+      <div className="w-1/2 flex flex-col items-center">
+        <img src={Bob} alt="Bob" className="w-24 h-24 mb-2 rounded-full" />
+        <h2 className="font-bold text-2xl">Bob</h2>
+      </div>
+    </div>
     </div>
   );
 }
