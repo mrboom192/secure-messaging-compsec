3b478dc2a07652fe21a9c16c77afe16dd05af3af
Victoria Rowe <victoriarowe@Mac.attlocal.net>
2025-04-28 20:53:47 -0500

    set up basic chat interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState('');
+  const [message, setMessage] = useState<{ciphertext: string; plaintext: string}[]>([]);
+
+  const sendMessage = async () => {
+    // later on will we encropt message here and send to backend
+    const plaintext = input;
+    const ciphertext = btoa(input); //this is just a placeholder for the actual encryption
+    //send to sever after setting up the backend
+    setMessage([...message, {ciphertext, plaintext}]);
+    setInput('');
+  };
 
   return (
-    <>
+    <div className="App">
+      <h1>Sercure Messenger </h1>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+        <button onClick={sendMessage}>Send</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <h2>Messages</h2>
+        {message.map((msg, idx) => (
+          <div key={idx}>
+            <p> <strong>Ciphertext:</strong> {msg.ciphertext}</p>
+            <p> <strong>Plaintext:</strong> {msg.plaintext}</p>
+          </div>
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
